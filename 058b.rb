27208ed8619fb653e91e6dd1fb97a660eea4bbc7
hirakumo <jinnbeizame2000@yahoo.co.jp>
2017-05-30 16:43:38 +0000

    2nd commit

--- 058b.rb
+++ 058b.rb
@@ -1,14 +1,10 @@
 o = gets.chomp.split("")
 e = gets.chomp.split("")
 
-# for i in 0..o.length-1 do
-#     print o[i]
+for i in 0..o.length-1 do
+    print o[i]
     
-#     if i <= e.length
-#         print e[i]
-#     end
-# end 
-
-puts o
-p o
-print o
+    if i <= e.length
+        print e[i]
+    end
+end 
