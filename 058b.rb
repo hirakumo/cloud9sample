88a14ef88d962f70ff8377af63bc68052eac3aac
hirakumo <jinnbeizame2000@yahoo.co.jp>
2017-05-30 16:16:46 +0000

    first commit

---
+++ 058b.rb
@@ -0,0 +1,14 @@
+o = gets.chomp.split("")
+e = gets.chomp.split("")
+
+# for i in 0..o.length-1 do
+#     print o[i]
+    
+#     if i <= e.length
+#         print e[i]
+#     end
+# end 
+
+puts o
+p o
+print o
